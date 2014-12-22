cd79d6ab4c215f0c4d047a15e44dac4883db0bd4
yuxiaopeng <uxiaopeng@gmail.com>
2014-12-22 16:59:35 +0800

    Initial Commit

---
+++ Adapter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Adapter
+//
+//  Created by 小鹏 on 14/12/22.
+//  Copyright (c) 2014年 ROC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
